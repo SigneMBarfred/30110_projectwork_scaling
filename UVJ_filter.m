542446b3d45729e2e3c76382b69fe94d426714be
ZephyrGodofWind <122529072+ZephyrGodofWind@users.noreply.github.com>
2023-03-08 14:57:34 +0100

    Add files via upload

--- UVJ_filter.m
+++ UVJ_filter.m
@@ -1,19 +1,51 @@
 
 clear; close all; clc;
-
+    
 %%
 data = load('seds_1.txt');
 t_data = readtable('seds_1.txt');
 
+%%
+
+CFHT1 = importdata('CFHT_CFH12k.B.dat');
+CFHT2 = importdata('CFHT_CFH12k.R.dat');
+Subaru = importdata('Subaru_CIAO.Ks.dat');
+NewJband = importdata('2MASS_2MASS.J.dat');
+
+CFHT1(:,2) = 10.^(CFHT1(:,2)); % Filter funktion
+CFHT2(:,2) = 10.^(CFHT2(:,2)); % Visible on log axes
+Subaru(:,2) = 10.^(Subaru(:,2));
+NewJband(:,2) = 10.^(NewJband(:,2));
+
+flux_min = 10^(-5);
+CFH1_max = max(CFHT1(:,2));
+CFH2_max = max(CFHT2(:,2));
+Subaru_max = max(Subaru(:,2));
+NewJband_max = max(NewJband(:,2));
+
+CFHT1(:,2) = (CFHT1(:,2)./CFH1_max); % Normalize
+CFHT2(:,2) = (CFHT2(:,2)./CFH2_max);
+Subaru(:,2) = (Subaru(:,2)./Subaru_max);
+NewJband(:,2) = (NewJband(:,2)./NewJband_max);
+
+CFHT1(:,1) = (CFHT1(:,1)./10000); % match wl units micron
+CFHT2(:,1) = (CFHT2(:,1)./10000);
+Subaru(:,1) = (Subaru(:,1)./10000);
+NewJband(:,1) = (NewJband(:,1)./10000);
+
 % mu meter
 lambda = data(:,1);
+
+%%
+
 
 % color vector
 Vcol = jet(12);
 
 figure
-hold on
 fig_1 =plot(lambda,data(:,2:13));
+
+hold on
 set(fig_1, {'color'}, num2cell(jet(12),2));
 set(gca, 'yScale', 'log')
 set(gca, 'xScale', 'log')
@@ -22,7 +54,12 @@ set(gca, 'XLim', [0 5]);
 xlabel('Wavelength [\mum]')
 ylabel('Flux [\muJy]')
 title('Flux as a function of wavelength')
-legend('G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',Location='best',Orientation='vertical')
+plot(CFHT1(:,1),CFHT1(:,2).*flux_min)
+plot(CFHT2(:,1),CFHT2(:,2).*flux_min)
+plot(Subaru(:,1),Subaru(:,2).*flux_min)
+plot(NewJband(:,1),NewJband(:,2).*flux_min)
+legend('G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','U-filter','V-filter','J-filter','New J',Location='southeastoutside',Orientation='vertical')
+
 hold off
 
 
@@ -58,6 +95,38 @@ JwlAve = sum(lambda(J))/size(lambda(J),1);
 
 L1 = [linspace(UwlAve,UwlAve,12),linspace(VwlAve,VwlAve,12),linspace(JwlAve,JwlAve,12)];
 
+%%
+
+% Filter polygons
+
+% U_x = transpose([(U_c-U_w),(U_c+U_w),(U_c+U_w),(U_c-U_w),(U_c-U_w)]);
+% U_y = transpose([0,0,flux_min*2/3,flux_min*2/3,0]);
+% P1 = [U_x,U_y];
+% U_pgon = polyshape(P1,'SolidBoundaryOrientation','ccw' );
+% [u_x,u_y] = centroid(U_pgon);
+% 
+% V_x = transpose([(V_c-V_w),(V_c+V_w),(V_c+V_w),(V_c-V_w),(V_c-V_w)]);
+% V_y = transpose([0,0,flux_min*2/3,flux_min*2/3,0]);
+% P2 = [V_x,V_y];
+% V_pgon = polyshape(P2,'SolidBoundaryOrientation','ccw' );
+% 
+% J_x = transpose([(J_c-J_w),(J_c+J_w),(J_c+J_w),(J_c-J_w)]);
+% J_y = transpose([0,0,flux_min*2/3,flux_min*2/3]);
+% P3 = [J_x,J_y];
+% J_pgon = polyshape(P3,'SolidBoundaryOrientation','ccw' );
+
+
+%%
+
+% % test polygon plot
+% 
+% plotU_pgon = figure;
+% plot(U_pgon,'FaceColor','[.5 0 .5]');
+% 
+% hold on
+% axis('equal')
+% text(u_x,u_y, sprintf('Polygon with centroid (%.2f, %.2f)', u_x, u_y), 'HorizontalAlignment','center', 'VerticalAlignment','middle')
+% hold off
 
 %%
 
@@ -76,13 +145,15 @@ set(gca, 'XLim', [0 5]);
 xlabel('Wavelength [\mum]')
 ylabel('Flux [\muJy]')
 title('Flux as a function of wavelength')
+plot(CFHT1(:,1),CFHT1(:,2).*flux_min,'Color',[0 0 0])
+plot(CFHT2(:,1),CFHT2(:,2).*flux_min,'Color','b')
+plot(Subaru(:,1),Subaru(:,2).*flux_min,'Color','r')
+plot(NewJband(:,1),NewJband(:,2).*flux_min,'Color','r')
 hold off
 
-leg1=legend(p1,'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','Location','best');
+leg1=legend(p1,'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','Location','southeastoutside');
 ah1=axes('position',get(gca,'position'),'visible','off');
 leg2=legend(ah1,[p2,p3,p4],'U band', 'V band','J band','Location','northeastoutside');set(leg2,'FontSize',9);
-% legendTitle(leg1, 'Broadband type' );
-% legendTitle(leg2, 'Galaxy name' );
 
 % saveas(f2,'Flux_wl_aveUVJ','fig');
 
@@ -96,8 +167,12 @@ m_V = 23.9 - 2.5*log10(Vave);
 m_J = 23.9 - 2.5*log10(Jave);
 
 % convert entire data to mag
+M = data(:,2:13);
+M(M==0) = NaN;
+mag = 23.9 - 2.5*log10(M);
+mag(mag==Inf)
 
-mag = 23.9 - 2.5*   log10(data(:,2:13));
+mag_max = 40 - (23.9 - 2.5*log10(min(CFHT1(:,2))));
 
 %%
 
@@ -112,13 +187,18 @@ p8 = scatter(L1(:,25:36),m_J,'filled',MarkerFaceColor=[1 0 0]);
 set(gca, 'xScale', 'log')
 set(gca, 'YDir','reverse')
 set(gca, 'XTick', [0.1 0.2 0.3 0.5 0.8 1.2 1.8 2.7 4]);
-set(gca, 'XLim', [0 5]);
+set(gca, 'XLim', [0.3 0.8]);
+set(gca, 'YLim', [18 40]);
 xlabel('Wavelength [\mum]')
 ylabel('AB magnitude')
 title('AB magnitude as a function of wavelength')
+plot(CFHT1(:,1),(23.9 - 2.5*log10(CFHT1(:,2)))+mag_max,'Color',[0 0 0])
+plot(CFHT2(:,1),(23.9 - 2.5*log10(CFHT2(:,2)))+mag_max,'Color','b')
+plot(Subaru(:,1),(23.9 - 2.5*log10(Subaru(:,2)))+mag_max,'Color','r')
+plot(NewJband(:,1),(23.9 - 2.5*log10(NewJband(:,2)))+mag_max,'Color','r')
 hold off
 
-leg3=legend(p5,'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','Location','best');
+leg3=legend(p5,'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','Location','southeastoutside');
 ah2=axes('position',get(gca,'position'),'visible','off');
 leg4=legend(ah2,[p6,p7,p8],'U band', 'V band','J band','Location','northeastoutside');set(leg4,'FontSize',9);
 
@@ -204,15 +284,15 @@ nUV = UV > 1.3;
 %eq2 VJ
 nVJ = VJ < 1.6;
 %eq3
-UVVJ = 0.88 * VJ+0.49;
+UVVJ = 0.88 * VJ+0.69;
 
 %%
-figure
-hold on
-plot(sort(VJ),sort(UVVJ))
-plot([0 0.58 1],[1.3 1.3 1.3])
-plot([1.6,1.6],[0,2.5])
-hold off
+% figure
+% hold on
+% plot(sort(VJ),sort(UVVJ))
+% plot([0 0.58 1],[1.3 1.3 1.3])
+% plot([1.6,1.6],[0,2.5])
+% hold off
 
 
 %%
@@ -233,33 +313,24 @@ yUVVJ = UVVJ;
 
 %%
 
-% Hældning
-
-a=(y2(1)-y1(1))/(x2(1)-x1(1));
-b=y2(1)-a*x2(1);
-
-oll = a*1.5+b;
-
-%%
-
-figure
-hold on
-plot(VJ,UVVJ)
-scatter(VJ,UVVJ)
-scatter(x1,y1)
-scatter(x2,y2)
-grid on
-hold off
+% figure
+% hold on
+% plot(VJ,UVVJ)
+% scatter(VJ,UVVJ)
+% scatter(x1,y1)
+% scatter(x2,y2)
+% grid on
+% hold off
 
 %%
 % Define QSGs boundary
 % 
-xv = transpose([0,0.9205,1.6,1.6,0,0]);
-yv = transpose([1.3,1.3,1.8980,2.5,2.5,1.3]);
+xv = transpose([0,x1(1),1.6,1.6,0,0]);
+yv = transpose([1.3,1.3,y2(1),2.5,2.5,1.3]);
 
 P = [xv,yv];
 pgon = polyshape(P,'SolidBoundaryOrientation','ccw' );
-plot(pgon)
+% plot(pgon)
 
 TFin = isinterior(pgon,VJ,UV);
 
@@ -283,23 +354,26 @@ set(gca, 'YLim', [0 2.5]);
 ylabel('U-V [AB_{mag}]')
 xlabel('V-J [AB_{mag}]')
 title('VUJ diagram')
-legend('G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',Location='best',Orientation='vertical')
 % fig_2 = plot(VJ(TFin),UV(TFin),'*'); % points inside
 % set(fig_2, {'color'}, Mcol(TFin,:));
 % fig_3 = plot(VJ(~TFin),UV(~TFin),'+'); % points outside
 % set(fig_1, {'color'}, Mcol(~TFin,:));
-scatter(VJ(1),UV(1),"MarkerFaceColor",Mcol(1,:));
-scatter(VJ(2),UV(2),"MarkerFaceColor",Mcol(2,:));
-scatter(VJ(3),UV(3),"MarkerFaceColor",Mcol(3,:));
-scatter(VJ(4),UV(4),"MarkerFaceColor",Mcol(4,:));
-scatter(VJ(5),UV(5),"MarkerFaceColor",Mcol(5,:));
-scatter(VJ(6),UV(6),"MarkerFaceColor",Mcol(6,:));
-scatter(VJ(7),UV(7),"MarkerFaceColor",Mcol(7,:));
-scatter(VJ(8),UV(8),"MarkerFaceColor",Mcol(8,:));
-scatter(VJ(9),UV(9),"MarkerFaceColor",Mcol(9,:));
-scatter(VJ(10),UV(10),"MarkerFaceColor",Mcol(10,:));
-scatter(VJ(11),UV(11),"MarkerFaceColor",Mcol(11,:));
-scatter(VJ(12),UV(12),"MarkerFaceColor",Mcol(12,:));
+scat1 = scatter(VJ(1),UV(1),"MarkerFaceColor",Mcol(1,:));
+scat2 = scatter(VJ(2),UV(2),"MarkerFaceColor",Mcol(2,:));
+scat3 = scatter(VJ(3),UV(3),"MarkerFaceColor",Mcol(3,:));
+scat4 = scatter(VJ(4),UV(4),"MarkerFaceColor",Mcol(4,:));
+scat5 = scatter(VJ(5),UV(5),"MarkerFaceColor",Mcol(5,:));
+scat6 = scatter(VJ(6),UV(6),"MarkerFaceColor",Mcol(6,:));
+scat7 = scatter(VJ(7),UV(7),"MarkerFaceColor",Mcol(7,:));
+scat8 = scatter(VJ(8),UV(8),"MarkerFaceColor",Mcol(8,:));
+scat9 = scatter(VJ(9),UV(9),"MarkerFaceColor",Mcol(9,:));
+scat10 = scatter(VJ(10),UV(10),"MarkerFaceColor",Mcol(10,:));
+scat11 = scatter(VJ(11),UV(11),"MarkerFaceColor",Mcol(11,:));
+scat12 = scatter(VJ(12),UV(12),"MarkerFaceColor",Mcol(12,:));
+
+legend([scat1,scat2,scat3,scat4,scat5,scat6,scat7,scat8,scat9,scat10, ...
+    scat11,scat12],'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10', ...
+    'G11','G12',Location='best',Orientation='vertical')
 hold off
 
 

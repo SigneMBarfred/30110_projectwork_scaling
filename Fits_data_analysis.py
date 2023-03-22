cc3f81d0ab8d58a3d3012cc6642a8eeb52590ee4
ZephyrGodofWind <122529072+ZephyrGodofWind@users.noreply.github.com>
2023-03-22 10:39:31 +0100

    Add files via upload

--- Fits_data_analysis.py
+++ Fits_data_analysis.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 # Load file
 # open it with memmap=True to prevent RAM storage issues.
-hdu_list = fits.open('COSMOS2020_flux_mag_zphot.fits', memmap=True)
+hdu_list = fits.open('COSMOS2020_flux_mag_zphot.fits')
 hdu_list.info()
 
 # Print column names
@@ -27,7 +27,7 @@ print(hdu_list[1].columns)
 
 
 # Convert data in to an astropy.table - Because it's more convenient
-evt_data = Table(hdu_list[1].data)
+evt_data = hdu_list[1].data
 
 # Display table
 evt_data
@@ -36,7 +36,9 @@ evt_data
 
 # Remove noise and unreliable values (-99, >300)
 evt_data = evt_data[evt_data != -99]
-#evt_data.remove_rows([])
+#evt_data.remove_rows([]
+
+
 # If statement to remove rows with higher values than 300
 
 
@@ -77,23 +79,27 @@ Cwl_GALEX_FUV = 1526
 Cwl_GALEX_NUV = 2307
 Cwl_F814W = 8333
 
-#lambda = np.array([Cwl_CFHT_u,Cwl_CFHT_ustar,Cwl_HSC_g,Cwl_HSC_r,Cwl_HSC_i,Cwl_HSC_z,Cwl_HSC_y,Cwl_UVISTA_Y,
- #         Cwl_UVISTA_J,Cwl_UVISTA_H,Cwl_UVISTA_Ks,Cwl_SC_IB427,Cwl_SC_IB464,Cwl_SC_IA484,Cwl_SC_IB505,
- #         Cwl_SC_IA527,Cwl_SC_IB574,Cwl_SC_IA624,Cwl_SC_IA679,Cwl_SC_IB709,Cwl_SC_IA738,Cwl_SC_IA767,
-  #        Cwl_SC_IB827,Cwl_SC_NB711,Cwl_SC_NB816,Cwl_IRAC_CH1,Cwl_IRAC_CH2,Cwl_SPLASH_CH2,Cwl_SPLASH_CH3,
-   #       Cwl_GALEX_FUV,Cwl_GALEX_NUV,Cwl_F814W])
+Lambda = np.array([Cwl_CFHT_u,Cwl_CFHT_ustar,Cwl_HSC_g,Cwl_HSC_r,Cwl_HSC_i,Cwl_HSC_z,Cwl_HSC_y,Cwl_UVISTA_Y,
+          Cwl_UVISTA_J,Cwl_UVISTA_H,Cwl_UVISTA_Ks,Cwl_SC_IB427,Cwl_SC_IB464,Cwl_SC_IA484,Cwl_SC_IB505,
+          Cwl_SC_IA527,Cwl_SC_IB574,Cwl_SC_IA624,Cwl_SC_IA679,Cwl_SC_IB709,Cwl_SC_IA738,Cwl_SC_IA767,
+          Cwl_SC_IB827,Cwl_SC_NB711,Cwl_SC_NB816,Cwl_IRAC_CH1,Cwl_IRAC_CH2,Cwl_SPLASH_CH2,Cwl_SPLASH_CH3,
+          Cwl_GALEX_FUV,Cwl_GALEX_NUV,Cwl_F814W])
 
 
 #### Plot 1 scatter(wl,flux) of 10 galaxies
-  
-a = [1, 3, 5, 7]
-b = [11, -2, 4, 19]
-plt.scatter(a, b)
+
+G10_flux = np.array([evt_data['CFHT_u_FLUX'][1:10],evt_data['CFHT_g_FLUX'][1:10],evt_data['CFHT_u_FLUX'][1:10]])
+
+G10_fluxerr = np.array([evt_data['GALEX_FUV_FLUXERR'][1:10],evt_data['GALEX_NUV_MAGERR'][1:10],evt_data['CFHT_u_FLUXERR'][1:10],evt_data['CFHT_g_FLUXERR'][1:10],evt_data['CFHT_r_FLUXERR'][1:10],
+                        evt_data['CFHT_i_FLUXERR'][1:10],evt_data['CFHT_z_FLUXERR'][1:10],evt_data['CFHT_y_FLUXERR'][1:10],
+                        evt_data['CFHT_y_FLUXERR'][1:10],])
+
+#plt.scatter(a, b)
   
 c = [1, 3, 2, 1]
   
-plt.errorbar(a, b, yerr=c, fmt="o")
-plt.show()
+#plt.errorbar(a, b, yerr=c, fmt="o")
+#plt.show()
 
 
 

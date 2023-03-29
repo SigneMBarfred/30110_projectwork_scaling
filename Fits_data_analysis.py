e81a7e66b2e01a3c1dc16c72780daa50e897448e
ZephyrGodofWind <122529072+ZephyrGodofWind@users.noreply.github.com>
2023-03-29 13:22:35 +0200

    Add files via upload

--- Fits_data_analysis.py
+++ Fits_data_analysis.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 10 12:50:55 2023
+Created on Sun Mar 26 17:58:28 2023
 
 @author: Nikolaj Lange Dons
 """
@@ -10,17 +10,24 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 
 #loading data as recarrays
-hdu = fits.open('COSMOS2020_flux_mag.fits')
+hdu = fits.open('COSMOS2020_best.fits',memmap=True)
 data = hdu[1].data
 
 
 # Remove noise and unreliable values
 for colname in data.names:
-    data = data[data[colname] >= -98]
-    data = data[data[colname] <= 1000]
+    data = data[data['mass'] > 10**8]
+    data = data[data['mass'] < 10**13]
+    data = data[data['z_phot'] != -1]
+    data = data[data['UVISTA_Ks_FLUX'] != -99]
+    data = data[data['CFHT_u_FLUX'] != -99]
+    # data = data[data['HSC_g_FLUX'] != -99]
+    data = data[data['HSC_r_FLUX'] != -99]
+    # data = data[data['HSC_i_FLUX'] != -99]
+    data = data[data['UVISTA_J_FLUX'] != -99]
+    
 
 # Define central wavelength (Cwl) in Ångstrom[Å] from used bands/filters from cosmos2020
-Cwl_GALEX_FUV = 1526
 Cwl_GALEX_NUV = 2307
 Cwl_CFHT_u = 3709
 Cwl_CFHT_ustar = 3858
@@ -41,7 +48,6 @@ Cwl_SC_IA767 = 7694
 Cwl_HSC_i = 7699
 Cwl_SC_NB816 = 8150
 Cwl_SC_IB827 = 8243
-Cwl_F814W = 8333
 Cwl_HSC_z = 8894
 Cwl_HSC_y = 9761
 Cwl_UVISTA_Y = 10216
@@ -54,7 +60,7 @@ Cwl_IRAC_CH2 = 45067
 Lambda = np.array([Cwl_CFHT_u,Cwl_CFHT_ustar,Cwl_HSC_g,Cwl_HSC_r,Cwl_HSC_i,Cwl_HSC_z,Cwl_HSC_y,Cwl_UVISTA_Y,
           Cwl_UVISTA_J,Cwl_UVISTA_H,Cwl_UVISTA_Ks,Cwl_SC_IB427,Cwl_SC_IB464,Cwl_SC_IA484,Cwl_SC_IB505,
           Cwl_SC_IA527,Cwl_SC_IB574,Cwl_SC_IA624,Cwl_SC_IA679,Cwl_SC_IB709,Cwl_SC_IA738,Cwl_SC_IA767,
-          Cwl_SC_IB827,Cwl_SC_NB711,Cwl_SC_NB816,Cwl_IRAC_CH1,Cwl_IRAC_CH2,Cwl_GALEX_FUV,Cwl_GALEX_NUV,Cwl_F814W])
+          Cwl_SC_IB827,Cwl_SC_NB711,Cwl_SC_NB816,Cwl_IRAC_CH1,Cwl_IRAC_CH2,Cwl_GALEX_NUV])
 
 #sorts lambda array from lowest to highest
 def selection_sort(Lambda):
@@ -62,7 +68,6 @@ def selection_sort(Lambda):
         swap = i + np.argmin(Lambda[i:])
         (Lambda[i], Lambda[swap]) = (Lambda[swap], Lambda[i])
     return Lambda
-Lambda = selection_sort(Lambda)
 
 #converts lambda from ångstrøm to micrometer
 Lambda = Lambda*10**-4
@@ -113,7 +118,7 @@ ax.set_yscale("log")
 ax.set_xlim(10**-1, 10**1)
 ax.set_ylim(10**-6, 10**2)
 ax.set_xlabel("Wavelength (µm)")
-ax.set_ylabel("Flux density (W/m2)")
+ax.set_ylabel("Flux density")
 ax.set_title("Flux Density against Wavelength of Galaxy 1")
 
 #initialize layout of galaxy 2
@@ -127,9 +132,9 @@ ax.errorbar(Lambda, G10_flux[:,1], yerr = G10_fluxerr[:,1],fmt = "o", alpha = 0.
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlim(10**-1, 10**1)
-ax.set_ylim(10**-1, 10**2)
+ax.set_ylim(10**-6, 10**2)
 ax.set_xlabel("Wavelength (µm)")
-ax.set_ylabel("Flux density (W/m2)")
+ax.set_ylabel("Flux density")
 ax.set_title("Flux Density against Wavelength of Galaxy 2")
 
 #initialize layout of G3
@@ -143,9 +148,9 @@ ax.errorbar(Lambda, G10_flux[:,2], yerr = G10_fluxerr[:,2],fmt = "o", alpha = 0.
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlim(10**-1, 10**1)
-ax.set_ylim(10**-1, 10**2)
+ax.set_ylim(10**-6, 10**2)
 ax.set_xlabel("Wavelength (µm)")
-ax.set_ylabel("Flux density (W/m2)")
+ax.set_ylabel("Flux density")
 ax.set_title("Flux Density against Wavelength of Galaxy 3")
 
 # Close FITS file so it won't use up excess memory

ffc22db36ab07de7b8581096923967be911726fd
ZephyrGodofWind <122529072+ZephyrGodofWind@users.noreply.github.com>
2023-04-16 15:41:46 +0200

    Add files via upload

--- UVJ_diagram_scatter.py
+++ UVJ_diagram_scatter.py
@@ -85,72 +85,72 @@ ax.set_title("UVJ diagram for galaxies at all redshifts", fontsize=18)
 
 ######################
 
-# UVJ diagram for cat1
-# 0.1 < z < 0.3
+# # UVJ diagram for cat1
+# # 0.1 < z < 0.3
 
-selection_cat1 = np.logical_and((data['z_phot'] > 0.1), (data['z_phot'] < 0.3), (data['CFHT_u_MAG'] < 50))
+# selection_cat1 = np.logical_and((data['z_phot'] > 0.1), (data['z_phot'] < 0.3), (data['CFHT_u_MAG'] < 50))
 
-UV_cat1 = U[selection_cat1[0,:]] - V[selection_cat1[0,:]]
-VJ_cat1 = V[selection_cat1[0,:]] - J[selection_cat1[0,:]]
+# UV_cat1 = U[selection_cat1[0,:]] - V[selection_cat1[0,:]]
+# VJ_cat1 = V[selection_cat1[0,:]] - J[selection_cat1[0,:]]
 
-fig1, ax = plt.subplots(figsize = (8, 6))
-hb1 = ax.hexbin(VJ_cat1, UV_cat1, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
-cb1 = fig1.colorbar(hb1, ax = ax)
-cb1.set_label("Number of galaxies")
-ax.plot([-4, 0.693], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
-ax.plot([1.6, 1.6], [2.098, 4], color = "g", lw = 5) #draws the vertical line
-ax.plot([0.693, 1.6], [1.3, 2.098], color = "g", lw = 5) #draws the tilted line
-ax.set_xlim(0,2.5)
-ax.set_ylim(0,2.5)
-ax.set_xlabel("V - J [AB mag]", fontsize=12)
-ax.set_ylabel("U - V [AB mag]", fontsize=12)
-ax.set_title("UVJ diagram for 0.1 < z < 0.3", fontsize=18)
+# fig1, ax = plt.subplots(figsize = (8, 6))
+# hb1 = ax.hexbin(VJ_cat1, UV_cat1, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
+# cb1 = fig1.colorbar(hb1, ax = ax)
+# cb1.set_label("Number of galaxies")
+# ax.plot([-4, 0.693], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
+# ax.plot([1.6, 1.6], [2.098, 4], color = "g", lw = 5) #draws the vertical line
+# ax.plot([0.693, 1.6], [1.3, 2.098], color = "g", lw = 5) #draws the tilted line
+# ax.set_xlim(0,2.5)
+# ax.set_ylim(0,2.5)
+# ax.set_xlabel("V - J [AB mag]", fontsize=12)
+# ax.set_ylabel("U - V [AB mag]", fontsize=12)
+# ax.set_title("UVJ diagram for 0.1 < z < 0.3", fontsize=18)
 
-######################
+# ######################
 
-# UVJ diagram for cat2
-# 0.9 < z < 1.1
+# # UVJ diagram for cat2
+# # 0.9 < z < 1.1
 
-selection_cat2 = np.logical_and((data['z_phot'] > 0.9), (data['z_phot'] < 1.1), (data['CFHT_u_MAG'] < 50))
+# selection_cat2 = np.logical_and((data['z_phot'] > 0.9), (data['z_phot'] < 1.1), (data['CFHT_u_MAG'] < 50))
 
-UV_cat2 = U[selection_cat2[0,:]] - V[selection_cat2[0,:]]
-VJ_cat2 = V[selection_cat2[0,:]] - J[selection_cat2[0,:]]
+# UV_cat2 = U[selection_cat2[0,:]] - V[selection_cat2[0,:]]
+# VJ_cat2 = V[selection_cat2[0,:]] - J[selection_cat2[0,:]]
 
-fig2, ax = plt.subplots(figsize = (8, 6))
-hb2 = ax.hexbin(VJ_cat2, UV_cat2, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
-cb2 = fig2.colorbar(hb2, ax = ax)
-cb2.set_label("Number of galaxies")
-ax.plot([-4.0, 0.81], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
-ax.plot([1.6, 1.6], [2.0, 4], color = "g", lw = 5) #draws the vertical line
-ax.plot([0.81, 1.6], [1.3, 2.0], color = "g", lw = 5) #draws the tilted line
-ax.set_xlim(0,2.5)
-ax.set_ylim(0,2.5)
-ax.set_xlabel("V - J [AB mag]", fontsize=12)
-ax.set_ylabel("U - V [AB mag]", fontsize=12)
-ax.set_title("UVJ diagram for 0.9 < z < 1.1", fontsize=18)
+# fig2, ax = plt.subplots(figsize = (8, 6))
+# hb2 = ax.hexbin(VJ_cat2, UV_cat2, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
+# cb2 = fig2.colorbar(hb2, ax = ax)
+# cb2.set_label("Number of galaxies")
+# ax.plot([-4.0, 0.81], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
+# ax.plot([1.6, 1.6], [2.0, 4], color = "g", lw = 5) #draws the vertical line
+# ax.plot([0.81, 1.6], [1.3, 2.0], color = "g", lw = 5) #draws the tilted line
+# ax.set_xlim(0,2.5)
+# ax.set_ylim(0,2.5)
+# ax.set_xlabel("V - J [AB mag]", fontsize=12)
+# ax.set_ylabel("U - V [AB mag]", fontsize=12)
+# ax.set_title("UVJ diagram for 0.9 < z < 1.1", fontsize=18)
 
-######################
+# ######################
 
-# UVJ diagram for cat3
-# 1.9 < z < 2.1
+# # UVJ diagram for cat3
+# # 1.9 < z < 2.1
 
-selection_cat3 = np.logical_and((data['z_phot'] > 1.9), (data['z_phot'] < 2.1), (data['CFHT_u_MAG'] < 50))
+# selection_cat3 = np.logical_and((data['z_phot'] > 1.9), (data['z_phot'] < 2.1), (data['CFHT_u_MAG'] < 50))
 
-UV_cat3 = U[selection_cat3[0,:]] - V[selection_cat3[0,:]]
-VJ_cat3 = V[selection_cat3[0,:]] - J[selection_cat3[0,:]]
+# UV_cat3 = U[selection_cat3[0,:]] - V[selection_cat3[0,:]]
+# VJ_cat3 = V[selection_cat3[0,:]] - J[selection_cat3[0,:]]
 
-fig3, ax = plt.subplots(figsize = (8, 6))
-hb3 = ax.hexbin(VJ_cat3, UV_cat3, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
-cb3 = fig3.colorbar(hb3, ax = ax)
-cb3.set_label("Number of galaxies")
-ax.plot([-4.0, 0.92], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
-ax.plot([1.6, 1.6], [1.9, 4], color = "g", lw = 5) #draws the vertical line
-ax.plot([0.92, 1.6], [1.3, 1.9], color = "g", lw = 5) #draws the tilted line
-ax.set_xlim(0,2.5)
-ax.set_ylim(0,2.5)
-ax.set_xlabel("V - J [AB mag]", fontsize=12)
-ax.set_ylabel("U - V [AB mag]", fontsize=12)
-ax.set_title("UVJ diagram for 1.9 < z < 2.1", fontsize=18)
+# fig3, ax = plt.subplots(figsize = (8, 6))
+# hb3 = ax.hexbin(VJ_cat3, UV_cat3, vmax = 50, cmap = "hot", gridsize = (173,100), mincnt = 0)
+# cb3 = fig3.colorbar(hb3, ax = ax)
+# cb3.set_label("Number of galaxies")
+# ax.plot([-4.0, 0.92], [1.3, 1.3], color = "g", lw = 5) #draws the horizontal line
+# ax.plot([1.6, 1.6], [1.9, 4], color = "g", lw = 5) #draws the vertical line
+# ax.plot([0.92, 1.6], [1.3, 1.9], color = "g", lw = 5) #draws the tilted line
+# ax.set_xlim(0,2.5)
+# ax.set_ylim(0,2.5)
+# ax.set_xlabel("V - J [AB mag]", fontsize=12)
+# ax.set_ylabel("U - V [AB mag]", fontsize=12)
+# ax.set_title("UVJ diagram for 1.9 < z < 2.1", fontsize=18)
 
 ##################
 # Q/SF
@@ -389,6 +389,62 @@ ax.set_xlim(0,4)
 ax.set_xlabel("Redshift z")
 ax.set_ylabel("number of galaxies")
 ax.set_title("Number of galaxies at redshift z")
+
+########################
+#UVJ diagram at each redshift
+
+# UVJ diagram for cat1,2,3 og 4
+idcat1 = np.logical_and((data['z_phot'] > 0), (data['z_phot'] < 0.5))
+idcat2 = np.logical_and((data['z_phot'] > 0.25), (data['z_phot'] < 0.75))
+idcat3 = np.logical_and((data['z_phot'] > 0.5), (data['z_phot'] < 1))
+idcat4 = np.logical_and((data['z_phot'] > 0.75), (data['z_phot'] < 1.25))
+
+UVcat1 = U[idcat1[0,:]] - V[idcat1[0,:]]
+VJcat1 = V[idcat1[0,:]] - J[idcat1[0,:]]
+UVcat2 = U[idcat2[0,:]] - V[idcat2[0,:]]
+VJcat2 = V[idcat2[0,:]] - J[idcat2[0,:]]
+UVcat3 = U[idcat3[0,:]] - V[idcat3[0,:]]
+VJcat3 = V[idcat3[0,:]] - J[idcat3[0,:]]
+UVcat4 = U[idcat4[0,:]] - V[idcat4[0,:]]
+VJcat4 = V[idcat4[0,:]] - J[idcat4[0,:]]
+
+figure, ax = plt.subplots(2, 2, sharex=(True), sharey=(True), figsize=(10,10) )
+
+# cat1
+hb1 = ax[0,0].hexbin(VJcat1, UVcat1, vmax = 50, cmap = "binary", mincnt = 0)
+# cb1 = figure.colorbar(hb1, ax = ax)
+# cb1.set_label("Number of galaxies")
+ax[0,0].set_xlim(0,2.5)
+ax[0,0].set_ylim(0,2.5)
+ax[0,0].set_xlabel("VJ", fontsize=10)
+ax[0,0].set_ylabel("UV", fontsize=10)
+ax[0,0].set_title("0 < z < 0.5", fontsize=14)
+
+# cat2
+hb2 = ax[0,1].hexbin(VJcat2, UVcat2, vmax = 50, cmap = "binary", mincnt = 0)
+ax[0,1].set_xlim(0,2.5)
+ax[0,1].set_ylim(0,2.5)
+ax[0,1].set_xlabel("VJ", fontsize=10)
+ax[0,1].set_ylabel("UV", fontsize=10)
+ax[0,1].set_title("0.25 < z < 0.75", fontsize=14)
+
+# cat3
+hb3 = ax[1,0].hexbin(VJcat3, UVcat3, vmax = 50, cmap = "binary", mincnt = 0)
+ax[1,0].set_xlim(0,2.5)
+ax[1,0].set_ylim(0,2.5)
+ax[1,0].set_xlabel("VJ", fontsize=10)
+ax[1,0].set_ylabel("UV", fontsize=10)
+ax[1,0].set_title("0.5 < z < 1", fontsize=14)
+
+# cat4
+hb4 = ax[1,1].hexbin(VJcat4, UVcat4, vmax = 50, cmap = "binary", mincnt = 0)
+ax[1,1].set_xlim(0,2.5)
+ax[1,1].set_ylim(0,2.5)
+ax[1,1].set_xlabel("VJ", fontsize=10)
+ax[1,1].set_ylabel("UV", fontsize=10)
+ax[1,1].set_title("0.75 < z < 1.25", fontsize=14)
+
+plt.show()
 
 # Close FITS file so it won't use up excess memory
 hdu.close()
